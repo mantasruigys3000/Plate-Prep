aa302352e4221db7f42a09c009cd991d30028c28
ollie <oliver232@live.co.uk>
2020-05-11 15:27:35 +0100

    added intent and result view

--- app/src/main/java/com/example/db_api/MainActivity.kt
+++ app/src/main/java/com/example/db_api/MainActivity.kt
@@ -1,18 +1,34 @@
 package com.example.db_api
 
 import android.app.DownloadManager
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
+
+    fun startIntent(view: View) {
+
+        val intent = Intent(this, SearchActivity::class.java)
+        startActivity(intent)
+
+
+    }
+
+
 }
